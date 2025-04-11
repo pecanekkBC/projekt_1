6920640529ccd47dcdab4e04c7dd1a8f71d58882
pecanekkBC <petervajgl@seznam.cz>
2025-04-11 20:55:34 +0200

    dalsi_zkouska

--- skript_1.py
+++ skript_1.py
@@ -1,1 +1,9 @@
-print("ahoooj")
+suda_cisla = list()
+
+for cislo in (1, 2, 3, 4, 5):
+    if cislo % 2 == 0: 
+        print("sudé číslo")
+    elif cislo % 2 != 0:
+        print("liché číslo!")     # pokud je hodnota v proměnné "cislo" sudá
+         
+
